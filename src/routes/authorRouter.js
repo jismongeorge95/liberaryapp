57583363a3005f3c0f654c48de1b8ae55afb8455
unknown <Administrator@PLLAB-11.ASIETDomain.com>
2019-11-20 16:10:11 +0530

    array

--- src/routes/authorRouter.js
+++ src/routes/authorRouter.js
@@ -1,35 +1,35 @@
-const express=require('express');
-const authorRouter=express.Router();
-var authors=[
+const express = require('express');
+const authorRouter = express.Router();
+var authors = [
     {
-        name:"apj abdul kalam",
-        age:"75",
-        place:"Ramesharam",
-        image:"/img/img1.jpeg"
+        name: "apj abdul kalam",
+        age: "75",
+        place: "Ramesharam",
+        image: "/img/img1.jpeg"
     },
     {
-        name:"kamalasuraya",
-        age:"60",
-        place:"kozhikode",
-        image:"/img/img2.jpg"
+        name: "kamalasuraya",
+        age: "60",
+        place: "kozhikode",
+        image: "/img/img2.jpg"
     },
     {
-        name:"onv kurip",
-        age:"72",
-        place:"alapuzha",
-        image:"/img/img3.jpg",
+        name: "onv kurip",
+        age: "72",
+        place: "alapuzha",
+        image: "/img/img3.jpg",
     },
 ]
 
-function router(nav){
+function router(nav) {
     authorRouter.route('/')
-.get((req,res)=>{
-    res.render('authors.ejs',
-    {
-        nav,
-        title:"Author"
+        .get((req, res) => {
+            res.render('authors.ejs',
+                {
+                    nav,authors,
+                    title: "Author"
+                });
         });
-});
-return authorRouter;
+    return authorRouter;
 }
-module.exports=router;
+module.exports = router;
